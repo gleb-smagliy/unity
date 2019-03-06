bb3c9e37e12cccf24508b9b588818994e6966d8c
Gleb Smagliy <glebsmag@gmail.com>
2019-03-06 23:33:34 +0300

    skip tests for the registration handler and commit handler

--- packages/core/__tests__/registration/command-handlers/commit-registration-handler.js
+++ packages/core/__tests__/registration/command-handlers/commit-registration-handler.js
@@ -1,26 +1,26 @@
 describe('ServiceRegistrationCommitCommandHander', () =>
 {
-  it('should check if there is a version specified using storage', () =>
+  it.skip('should check if there is a version specified using storage', () =>
   {
     throw new Error();
   });
 
-  it('should return failure if storage returns failure', () =>
+  it.skip('should return failure if storage returns failure', () =>
   {
     throw new Error();
   });
 
-  it('should set stable tag to the specified version using storage', () =>
+  it.skip('should set stable tag to the specified version using storage', () =>
   {
     throw new Error();
   });
 
-  it('should return failure and release lock if tag updating failed', () =>
+  it.skip('should return failure and release lock if tag updating failed', () =>
   {
     throw new Error();
   });
 
-  it('should release lock according to version', () =>
+  it.skip('should release lock according to version', () =>
   {
     throw new Error();
   });
--- packages/core/__tests__/registration/command-handlers/registration-handler.test.js
+++ packages/core/__tests__/registration/command-handlers/registration-handler.test.js
@@ -1,3 +1,5 @@
+import { ServiceRegistrationCommandHander } from "../../../src/registration/command-handlers/registration-handler";
+
 describe('ServiceRegistrationCommandHander', () =>
 {
   it('should take a lock to register a service', () =>
@@ -5,112 +7,112 @@ describe('ServiceRegistrationCommandHander', () =>
     throw new Error();
   });
 
-  it('should return a failure if lock is already acquired', () =>
+  it.skip('should return a failure if lock is already acquired', () =>
   {
     throw new Error();
   });
 
-  it('should return failure and release lock if specified schema builder is not present in options', () =>
+  it.skip('should return failure and release lock if specified schema builder is not present in options', () =>
   {
     throw new Error();
   });
 
-  it('should request schema from schema builder according to passed command', () =>
+  it.skip('should request schema from schema builder according to passed command', () =>
   {
     throw new Error();
   });
 
-  it('should return failure and release lock if schema builder returns failure', () =>
+  it.skip('should return failure and release lock if schema builder returns failure', () =>
   {
     throw new Error();
   });
 
-  it('should request version by stable tag from storage', () =>
+  it.skip('should request version by stable tag from storage', () =>
   {
     throw new Error();
   });
 
-  it('should return failure and release lock if cannot take a version by stable tag from storage', () =>
+  it.skip('should return failure and release lock if cannot take a version by stable tag from storage', () =>
   {
     throw new Error();
   });
 
-  it('should take services by tagged as stable version', () =>
+  it.skip('should take services by tagged as stable version', () =>
   {
     throw new Error();
   });
 
-  it('should return failure and release lock if cannot take a services', () =>
+  it.skip('should return failure and release lock if cannot take a services', () =>
   {
     throw new Error();
   });
 
-  it('should request service metadata from metadata builder by specified schema builder', () =>
+  it.skip('should request service metadata from metadata builder by specified schema builder', () =>
   {
     throw new Error();
   });
 
-  it('should return failure and release lock if metadata builder returns failure', () =>
+  it.skip('should return failure and release lock if metadata builder returns failure', () =>
   {
     throw new Error();
   });
 
-  it('should transform schemas using schema transformers', () =>
+  it.skip('should transform schemas using schema transformers', () =>
   {
     throw new Error();
   });
 
-  it('should extract metadata using metadata extractors for extension builders', () =>
+  it.skip('should extract metadata using metadata extractors for extension builders', () =>
   {
     throw new Error();
   });
 
-  it('should return failure and release lock if extension builder metadata extractor returns failure', () =>
+  it.skip('should return failure and release lock if extension builder metadata extractor returns failure', () =>
   {
     throw new Error();
   });
 
-  it('should extract metadata using metadata extractors for gateway transformers', () =>
+  it.skip('should extract metadata using metadata extractors for gateway transformers', () =>
   {
     throw new Error();
   });
 
-  it('should return failure and release lock if gateway transformer metadata extractor returns failure', () =>
+  it.skip('should return failure and release lock if gateway transformer metadata extractor returns failure', () =>
   {
     throw new Error();
   });
 
-  it('should save new services using storage', () =>
+  it.skip('should save new services using storage', () =>
   {
     throw new Error();
   });
 
-  it('should assign a new version to the services using versioning strategy from options', () =>
+  it.skip('should assign a new version to the services using versioning strategy from options', () =>
   {
     throw new Error();
   });
 
-  it('should return failure and release lock if services could not be saved', () =>
+  it.skip('should return failure and release lock if services could not be saved', () =>
   {
     throw new Error();
   });
 
-  it('should return failure and release lock if services could not be saved', () =>
+  it.skip('should return failure and release lock if services could not be saved', () =>
   {
     throw new Error();
   });
 
-  it('should update lock assigning new version to it', () =>
+  it.skip('should update lock assigning new version to it', () =>
   {
     throw new Error();
   });
 
-  it('should return failure and release lock if lock version assigning fails', () =>
+  it.skip('should return failure and release lock if lock version assigning fails', () =>
   {
     throw new Error();
   });
 
-  it('should return new version', () =>
+  it.skip('should return new version', () =>
   {
     throw new Error();
   });
