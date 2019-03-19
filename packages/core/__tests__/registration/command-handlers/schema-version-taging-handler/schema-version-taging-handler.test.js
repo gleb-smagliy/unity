516358014eddcd47610ca2ed8d9a0afaf7c792b3
Gleb Smagliy <glebsmag@gmail.com>
2019-03-20 00:45:04 +0300

    changed existing and added some new tests for version tagging handler

---
+++ packages/core/__tests__/registration/command-handlers/schema-version-taging-handler/schema-version-taging-handler.test.js
@@ -0,0 +1,32 @@
+describe('ServiceRegistrationCommitCommandHander', () =>
+{
+  it.skip('should check if there are services of specified version using storage', async () =>
+  {
+    throw new Error();
+  });
+
+  it.skip('should return failure if storage returns failure', async () =>
+  {
+    throw new Error();
+  });
+
+  it.skip('should set specified tag to the version using storage', async () =>
+  {
+    throw new Error();
+  });
+
+  it.skip('should release lock if tagging is successful', async () =>
+  {
+    throw new Error();
+  });
+
+  it.skip('should return lock state in case of success', async () =>
+  {
+    throw new Error();
+  });
+
+  it.skip('should return failure and does not release lock if tag upserting failed', async () =>
+  {
+    throw new Error();
+  });
+});
